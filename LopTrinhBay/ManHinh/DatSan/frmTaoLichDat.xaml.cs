2cc6d2e403819c204305db39fc3c471b2a0d056d
Tên của bạn <twgdisayhi@gmail.com>
2025-10-14 03:11:30 +0700

    Thêm frmTaoLichDat

---
+++ LopTrinhBay/ManHinh/DatSan/frmTaoLichDat.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace QuanLiSanCauLong.LopTrinhBay.ManHinh.DatSan
+{
+    /// <summary>
+    /// Interaction logic for frmTaoLichDat.xaml
+    /// </summary>
+    public partial class frmTaoLichDat : Window
+    {
+        public frmTaoLichDat()
+        {
+            InitializeComponent();
+        }
+    }
+}
