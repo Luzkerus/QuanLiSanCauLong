286bb2e32aef8b9350ec688fdd8833d3195ef00d
thien tai <thien@DESKTOP-TJ4DRND>
2025-11-11 12:47:48 +0700

    ucbaocao

--- LopTrinhBay/ManHinh/BaoCao/ucBaoCao.xaml.cs
+++ LopTrinhBay/ManHinh/BaoCao/ucBaoCao.xaml.cs
@@ -11,7 +11,6 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
-using System.Windows.Controls;
 
 namespace QuanLiSanCauLong.LopTrinhBay.ManHinh.BaoCao
 {
@@ -19,7 +18,7 @@ namespace QuanLiSanCauLong.LopTrinhBay.ManHinh.BaoCao
     {
         public ucBaoCao()
         {
-          //InitializeComponent(); // ← KHÔNG được comment
+          InitializeComponent(); // ← KHÔNG được comment
         }
     }
 }
