18e23eddfdc88e970e8f83a21e743072099a8cf2
Tên của bạn <twgdisayhi@gmail.com>
2025-11-13 00:55:11 +0700

    liên kết frm Đặt sân trong nút ở TQ

--- LopTrinhBay/ManHinh/TongQuan/ucTongQuan.xaml.cs
+++ LopTrinhBay/ManHinh/TongQuan/ucTongQuan.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using QuanLiSanCauLong.LopTrinhBay.ManHinh.DatSan;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,16 +13,25 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
+using QuanLiSanCauLong.LopTrinhBay.ManHinh.DatSan;
+
 namespace QuanLiSanCauLong.LopTrinhBay.ManHinh.TongQuan
 {
-    /// <summary>
-    /// Interaction logic for ucTongQuan.xaml
-    /// </summary>
     public partial class ucTongQuan : UserControl
     {
         public ucTongQuan()
         {
             InitializeComponent();
+        }
+
+        private void btnMoDatSan_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            e.Handled = true;
+            var win = new frmTaoLichDat
+            {
+                Owner = Window.GetWindow(this)
+            };
+            win.ShowDialog();
         }
     }
 }
