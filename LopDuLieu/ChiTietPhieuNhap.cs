aa164775da08544ec02c6d13b8742fdc5ec044c7
thien tai <thien@LUZKER>
2025-11-22 20:14:39 +0700

    Xong nhap hang

---
+++ LopDuLieu/ChiTietPhieuNhap.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLiSanCauLong.LopDuLieu
+{
+    public class ChiTietPhieuNhap
+    {
+        public string MaChiTiet { get; set; }   
+        public string MaHang { get; set; }
+        public string TenHang { get; set; }
+        
+        public string DVT { get; set; }
+        public int SoLuong { get; set; }
+        public decimal GiaNhap { get; set; }
+        public decimal ChietKhau { get; set; }
+        public decimal ChietKhauTien
+        {
+            get
+            {
+                return SoLuong * GiaNhap * (ChietKhau / 100);
+            }
+        }
+        public decimal VAT { get; set; } = 5;
+        public string SoLo { get; set; }
+        public DateTime HSD { get; set; }
+
+        public decimal ThanhTien
+        {
+            get
+            {
+                decimal tienTruocThue = SoLuong * GiaNhap * (1 - ChietKhau / 100);
+                decimal tienVAT = tienTruocThue * (VAT / 100);
+                return tienTruocThue + tienVAT;
+            }
+        }
+    }
+}
---
+++ LopTruyCapDuLieu/PhieuNhapDAL.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using QuanLiSanCauLong.LopDuLieu;
+
+namespace QuanLiSanCauLong.LopTruyCapDuLieu
+{
+    public class PhieuNhapDAL
+    {
+        private readonly string connectionString;
+
+        public PhieuNhapDAL()
+        {
+            ConnectStringDAL connect = new ConnectStringDAL();
+            connectionString = connect.GetConnectionString();
+        }
+
+        public void LuuPhieu(PhieuNhap phieu)
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                string sql = @"
+                INSERT INTO PhieuNhap (SoPhieu,NhaCungCap, NgayNhap, GhiChu, TongTien)
+                VALUES (@SoPhieu,@NhaCungCap, @NgayNhap, @GhiChu, @TongTien)";
+
+                SqlCommand cmd = new SqlCommand(sql, conn);
+
+                cmd.Parameters.AddWithValue("@SoPhieu", phieu.SoPhieu);
+                cmd.Parameters.AddWithValue("@NhaCungCap", phieu.NhaCungCap ?? (object)DBNull.Value);
+                cmd.Parameters.AddWithValue("@NgayNhap", phieu.NgayNhap);
+                cmd.Parameters.AddWithValue("@GhiChu", phieu.GhiChu ?? (object)DBNull.Value);
+                cmd.Parameters.AddWithValue("@TongTien", phieu.TongTien);
+
+                conn.Open();
+                cmd.ExecuteNonQuery();
+            }
+        }
+        public string LaySoPhieuLonNhat(string prefix)
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                string sql = @"SELECT TOP 1 SoPhieu
+                       FROM PhieuNhap
+                       WHERE SoPhieu LIKE @Prefix + '%'
+                       ORDER BY SoPhieu DESC";
+
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                cmd.Parameters.AddWithValue("@Prefix", prefix);
+
+                conn.Open();
+                var result = cmd.ExecuteScalar();
+
+                return result?.ToString();
+            }
+        }
+
+    }
+
+}
